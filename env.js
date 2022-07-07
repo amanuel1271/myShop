9174f7bd76b119b53a1ef99a3db89ac9c837f95a
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-07-08 01:30:24 +0900

    Add files via upload

---
+++ env.js
@@ -0,0 +1,5 @@
+process.env.MONGO_USER="amanu"
+process.env.MONGO_PASSWORD="ZCiqliv5OyJnXaWy"
+process.env.MONGO_DEFAULT_DATABASE="shop"
+process.env.STRIPE_KEY="sk_test_51LIbEuE5tiARmcipvFCR7aziAmg2gDDFu98bE3xLe0U6rq8Spr8pSXn94jl5azMpSqEGMN9hVEDXO00H6ulzxKpw00WOQTzVfP"
+process.env.NODE_ENV="production"
