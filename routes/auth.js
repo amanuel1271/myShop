305796cc8d82819cd182c2ab304ab6470716ee1c
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-07-09 16:10:55 +0900

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -32,10 +32,6 @@ router.post(
       .isEmail()
       .withMessage('Please enter a valid email.')
       .custom((value, { req }) => {
-        // if (value === 'test@test.com') {
-        //   throw new Error('This email address if forbidden.');
-        // }
-        // return true;
         return User.findOne({ email: value }).then(userDoc => {
           if (userDoc) {
             return Promise.reject(
@@ -65,13 +61,5 @@ router.post(
 );
 
 router.post('/logout', authController.postLogout);
-
-// router.get('/reset', authController.getReset);
-
-// router.post('/reset', authController.postReset);
-
-// router.get('/reset/:token', authController.getNewPassword);
-
-// router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
